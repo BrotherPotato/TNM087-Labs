9fcf1e14c3b60147e9887c11597d85d7e85e1643
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2023-11-02 12:01:58 +0100

    s

--- Lab1/Prep1.m
+++ Lab1/Prep1.m
@@ -1,1 +1,12 @@
 Image = imread(".\Lab1_Images\book-cover.tif")
+max(Image(:))
+min(Image(:))
+%
+Image2=Image/16;
+max(Image(:))
+min(Image(:))
+Image3=Image2*16;
+max(Image(:))
+min(Image(:))
+A = uint8(0:255)
+B = (A/16)*16
