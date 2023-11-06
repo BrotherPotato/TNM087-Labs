e6c7ec6fd48de86f69ba7ea838d390a472d73395
BrotherPotato <59019335+BrotherPotato@users.noreply.github.com>
2023-11-06 11:48:34 +0100

    h

--- Lab1/TestScript.m
+++ Lab1/TestScript.m
@@ -1,12 +1,13 @@
 % Test 1.2 
 clear
 clc
-%figure(1)
-%GammaCorrection('.\Lab1_Images\aerialview-washedout.tif', 1, 0.5, 1)
+figure(1)
+GammaCorrection('.\Lab1_Images\aerialview-washedout.tif', 1, 0.5, 1)
 
 %figure(2)
 %imhist(GammaCorrection('.\Lab1_Images\aerialview-washedout.tif', 1, 0, 0.5))
 
 
 % Test 1.3
-LevelSlicing('.\Lab1_Images\RGBflower.tif', '.\Lab1_Images\IRflower.tif', 0.1)
+%figure(3)
+%LevelSlicing('.\Lab1_Images\RGBflower.tif', '.\Lab1_Images\IRflower.tif', 0.1)
