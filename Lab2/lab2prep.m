bfa0f8c242cc0f2f7194251ce6d5def157dbde72
Max Wiklundh <wiklundhmax@gmail.com>
2023-11-16 16:19:43 +0100

    s

--- Lab2/lab2prep.m
+++ Lab2/lab2prep.m
@@ -23,7 +23,27 @@ imwrite(Image3, ".\Lab2_SavedImages\P3.tif")
 impulse21 = zeros(21);
 impulse21(11,11) = 1;
 highPass21 = impulse21 - boxKernerl21;
+mean(highPass21(:))
 Image4 = imfilter(Image, highPass21, "replicate");
 figure(4)
 imshow(Image4)
 imwrite(Image4, ".\Lab2_SavedImages\P6.tif")
+% P8
+Image5 = Image4 + Image;
+figure(5)
+imshow(Image5)
+imwrite(Image5, ".\Lab2_SavedImages\P5.tif")
+% 2
+Sobx = [-1 -2 -1; 0 0 0; 1 2 1];
+Soby = [-1 0 1; -2 0 2; -1 0 1];
+% 9
+Image6 = filter2(Sobx, Image);
+figure(6)
+imshow(Image6)
+imwrite(Image6, ".\Lab2_SavedImages\P9.tif")
+% 10 
+Image7 = filter2(Soby, Image);
+figure(7)
+imshow(Image7)
+imwrite(Image7, ".\Lab2_SavedImages\P10.tif")
+
